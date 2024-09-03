dc1d37db333ac5d07ba9de1dbde47f99d29cb090
ytn <mail@ytn86.net>
2024-09-03 16:58:38 +0900

    :zap: initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# HTML2PDF-vuln-demo
+## 概要
+HTMLをベースとするPDF生成処理に発生するSSRFやLFIの脆弱性に関するデモアプリケーション
+
+
+## 起動方法
+```bash
+$ cd wkhtmltopdf
+$ docker compose up
+```
+
+## URL
+http://localhost:5000
+
---
+++ wkhtmltopdf/app/src/app.py
@@ -0,0 +1,42 @@
+from flask import (
+    Flask,
+    request,
+    render_template,
+    send_file
+)
+
+import uuid
+import subprocess
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/invoice', methods=['POST'])
+def invoice_pdf():
+
+    file_id = str(uuid.uuid4())
+
+    invoice_to = request.form.get('invoice_to') or ''
+    template = open('./templates/pdf_template.html').read().replace('###to###', invoice_to)
+
+    cmd = ['/usr/local/bin/wkhtmltopdf', '-', f'./data/{file_id}.pdf']
+    subprocess.run(cmd, timeout=5, input=template.encode(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    return send_file(f'data/{file_id}.pdf', download_name='invoice.pdf')
+
+
+@app.route('/internal', methods=['GET'])
+def internal():
+    if request.remote_addr != '127.0.0.1':
+        return 'internal access only'
+
+    return 'This is an internal page'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
